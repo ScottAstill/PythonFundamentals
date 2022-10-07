9610b8274ab953d539b517df31a0b38daf6d95cd
Scott Astill <ScottAstill@mail.weber.edu>
2022-10-06 18:10:47 -0600

    init repo

---
+++ numbers.py
@@ -0,0 +1,18 @@
+import random
+
+myInteger = 1923
+secondInteger = 1978
+myRandomYear = random.randint(1974, 2022)
+
+timePassed = myRandomYear - myInteger
+print(myRandomYear)
+print(timePassed)
+
+applePrice = 1.79
+orangePrice = 1.25
+bananaPriceInt = 2
+bananaPriceFloat = float(bananaPriceInt)
+
+bill = applePrice + orangePrice + bananaPriceInt
+print(bill, "\n")
+
